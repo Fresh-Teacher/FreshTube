91b99de3b46a7346c3148b4e092f70aef7913665
Fresh-Teacher <freshteacheruganda256@gmail.com>
2023-11-27 21:59:04 -0800

    modify page

--- static/js/app.ce013b4404c6229702e3.js
+++ static/js/app.ce013b4404c6229702e3.js
@@ -18,65 +18,20 @@ webpackJsonp(
             views: 2830,
             duration: "3 days ago",
           },
-          {
-            id: 2,
-            title: "Dope Tech: Camera Robots!",
-            thumbnail:
-              "https://i.ytimg.com/vi/UIwdCN4dV6w/hqdefault.jpg?sqp=-oaymwEZCNACELwBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLDhlan32jHSvicGZezDFPjAOdXGUA",
-            youtubeURL: "https://www.youtube.com/embed/UIwdCN4dV6w",
-            creator: "Fresh Teacher",
-            likes: 0,
-            views: 0,
-            duration: "2 days ago",
-          },
-          {
-            id: 3,
-            title:
-              "MY NANNY DRAMA | I'VE HAD 4 WITHIN 6 MONTHS | HOUSE HELP DIARIES",
-            thumbnail: "http://img.youtube.com/vi/KPVfCZST84U/0.jpg",
-            youtubeURL: "https://www.youtube.com/embed/KPVfCZST84U",
-            creator: "SiSi Yemmie",
-            likes: 0,
-            views: 0,
-            duration: "3 weeks ago",
-          },
-          {
-            id: 4,
-            title: "iPhone 8 Plus vs iPhone 7 Plus Full Comparison",
-            thumbnail: "http://img.youtube.com/vi/am3i3bPMWXM/0.jpg",
-            youtubeURL: "https://www.youtube.com/embed/am3i3bPMWXM",
-            creator: "Fresh Teacher",
-            likes: 0,
-            views: 0,
-            duration: "1 week ago",
-          },
-          {
-            id: 5,
-            title: "The Apple Ecosystem: Explained!",
-            thumbnail:
-              "https://i.ytimg.com/vi/KB4_WIPE7vo/hqdefault.jpg?sqp=-oaymwEZCNACELwBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLCCxXm7aoPShOwON74nhMlGYMUkHw",
-            youtubeURL: "https://www.youtube.com/embed/KB4_WIPE7vo",
-            creator: "Fresh Teacher",
-            likes: 0,
-            views: 0,
-            duration: "3 weeks ago",
-          },
-          {
-            id: 6,
-            title: "Talking Tech with Neil deGrasse Tyson!",
-            thumbnail:
-              "https://i.ytimg.com/vi/pqQrL1K0Z5g/hqdefault.jpg?sqp=-oaymwEZCNACELwBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLA5hTiwkz4Tr1w1hSMhPlwtmQeyYw",
-            youtubeURL: "https://www.youtube.com/embed/pqQrL1K0Z5g",
-            creator: "Fresh Teacher",
-            likes: 0,
-            views: 0,
-            duration: "3 days ago",
-          },
         ],
         o = {
           name: "VideoPlayer",
           data: function () {
-            return { videos: a, activeVideo: a[0] };
+            return {
+              videos: a,
+              activeVideo: a[0],
+              comments: [
+                { id: 1, user: "JohnDoe", content: "Great video!" },
+                { id: 2, user: "JaneSmith", content: "Awesome content!" },
+              ],
+              newCommentUser: "",
+              newCommentContent: "",
+            };
           },
           methods: {
             chooseVideo: function (e) {
@@ -84,6 +39,10 @@ webpackJsonp(
             },
             addLike: function () {
               this.activeVideo.likes += 1;
+            },
+            addComment: function (user, content) {
+              const newComment = { id: this.comments.length + 1, user, content };
+              this.comments.push(newComment);
             },
           },
         },
@@ -218,4 +177,3 @@ webpackJsonp(
   },
   ["NHnr"]
 );
-//# sourceMappingURL=app.ce013b4404c6229702e3.js.map
